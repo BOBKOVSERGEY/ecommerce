7ea59529b48d26861b810b644b1c00ae06101dd3
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2020-02-25 12:25:50 +0300

    Create a roures

---
+++ bootstrap/init.php
@@ -0,0 +1,10 @@
+<?php
+
+// init session
+if (!isset($_SESSION)) session_start();
+
+// load environment variable
+require_once __DIR__ . '/../app/config/_env.php';
+require_once __DIR__ . '/../app/routing/routes.php';
+
+new \App\RouteDispatcher($router);
