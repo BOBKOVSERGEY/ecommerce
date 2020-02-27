dc6ac81c0dd41b2f6a6782208507a617bc63b6cd
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2020-02-27 10:13:44 +0300

    Start creatind admin panel

--- app/routing/routes.php
+++ app/routing/routes.php
@@ -2,3 +2,6 @@
 
 $router = new AltoRouter();
 $router->map('GET', '/', 'App\Controllers\IndexController@show', 'home');
+
+// for admin routs
+$router->map('GET', '/admin', 'App\Controllers\Admin\DashboardController@index', 'admin_dashboard');
---
+++ resources/views/admin/layout/base.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin Panel - @yield('title')</title>
+    <link rel="stylesheet" href="/css/main.css">
+</head>
+<body>
+<div class="off-canvas position-left reveal-for-large" id="offCanvas" data-off-canvas>
+
+    <!-- Close button -->
+    <button class="close-button" aria-label="Close menu" type="button" data-close>
+        <span aria-hidden="true">&times;</span>
+    </button>
+
+    <!-- Menu -->
+    <ul class="vertical menu">
+        <li><a href="#">Foundation</a></li>
+        <li><a href="#">Dot</a></li>
+        <li><a href="#">ZURB</a></li>
+        <li><a href="#">Com</a></li>
+        <li><a href="#">Slash</a></li>
+        <li><a href="#">Sites</a></li>
+    </ul>
+
+</div>
+
+<div class="off-canvas-content" data-off-canvas-content>
+    @yield('content')
+</div>
+<script style="/js/lib.js"></script>
+<script style="/js/main.js"></script>
+</body>
+</html>
